033ca3616f71dddc7a6be8cce555deb436f4e198
dincengincan <engincandinc@gmail.com>
2019-12-10 21:07:47 +0300

    Updates

---
+++ precache-manifest.a8ac0d6cd9a7989d774ec6588c641b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462b16088bd7275fc951214a754725fd",
+    "url": "/burger-builder-react/index.html"
+  },
+  {
+    "revision": "c1eda5368b28157777b2",
+    "url": "/burger-builder-react/static/css/main.dd85a4ed.chunk.css"
+  },
+  {
+    "revision": "dd4809538cbfd7ee8955",
+    "url": "/burger-builder-react/static/js/2.7a416f70.chunk.js"
+  },
+  {
+    "revision": "c1eda5368b28157777b2",
+    "url": "/burger-builder-react/static/js/main.200d9d65.chunk.js"
+  },
+  {
+    "revision": "4e48f9c0ee0563b6cdc7",
+    "url": "/burger-builder-react/static/js/runtime-main.143452c8.js"
+  }
+]);
